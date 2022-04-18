378d0865c35445fce56060c0caf5593d86e6137d
Paphana Yiwsiw <paphana.yiwsiw@gmail.com>
2022-04-18 22:37:30 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 "use strict"; 
 
 var express = require("express"),
+	// https = require("https"),
 	http = require("http"),
+	// fs = require("fs"),
 	app;
 
 app = express();
+
+// var options = {
+// 	key: fs.readFileSync(__dirname + '/cert/server.key'),
+// 	cert: fs.readFileSync(__dirname + '/cert/server.cert')
+// };
+
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
 const path = __dirname + "/client";
 app.use(express.static(path));
-http.createServer(app).listen(process.env.PORT || 8080);
-console.log('Server started');
+http.createServer(app).listen(80);
+// https.createServer(options, app).listen(443);
+console.log('Server started at http://localhost and https://localhost');
 
 // set up our routes
 app.get("/hello", function (req, res) {
